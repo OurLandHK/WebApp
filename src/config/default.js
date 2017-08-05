309fecd34acb868542d0b858c17ebdeffb3aed87
khmak3 <pegasus.mak@gmail.com>
2017-08-05 15:45:05 +0800

    Add Development fb group
    Use Development group as default

--- src/config/default.js
+++ src/config/default.js
@@ -7,7 +7,8 @@ let config = {
     xfbml: true,
     version: 'v2.10'
   },
-  fbGroupId: '244493866025075'
+  // fbGroupId: '244493866025075' // Production https://www.facebook.com/groups/OurLandHK/?fref=nf
+  fbGroupId: '264191847414716' // For Development https://www.facebook.com/groups/264191847414716/ 
 };
 
 
