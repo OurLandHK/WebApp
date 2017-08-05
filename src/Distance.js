fff371f25732b8d8411a7554d83fa5811189c1f8
Kalun Wong <calendarw@gmail.com>
2017-08-05 17:57:23 +0800

    Add distance

---
+++ src/Distance.js
@@ -0,0 +1,19 @@
+function distance(lon1, lat1, lon2, lat2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+    var dLon = (lon2-lon1).toRad(); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return Math.round(d);
+}
+
+if (typeof(Number.prototype.toRad) === "undefined") {
+  Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+  }
+}
+
+export default distance;
--- src/MessageView.js
+++ src/MessageView.js
@@ -2,18 +2,47 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBlock, CardTitle, CardSubtitle, Button } from 'reactstrap';
 import * as firebase from 'firebase';
 import ProgressiveCardImg from './ProgressiveCardImg';
+import getLocation from './Location';
+import distance from './Distance.js';
 
 class MessageView extends Component {
   constructor(props) {
     super(props);
+    this.state = {lat: 0, lon: 0};
+    this.successCallBack = this.successCallBack.bind(this);
+  }
+
+  componentDidMount() {
+    getLocation(this.successCallBack, this.errorCallBack, this.notSupportedCallback);
+  }
+
+  notSupportedCallBack() {
+    console.log('Disabled');
+  }
+
+  successCallBack(pos) {
+    console.log('Your current position is:');
+    console.log('Latitude : ' + pos.coords.latitude);
+    console.log('Longitude: ' + pos.coords.longitude);
+    console.log('More or less ' + pos.coords.accuracy + 'meters.'); 
+    this.setState({ lat: pos.coords.latitude, lon: pos.coords.longitude}); 
+  }
+
+  errorCallBack(error) {
+    console.warn('ERROR(${err.code}): ${err.message}');
   }
 
   render() {
     var m = this.props.message;
     console.log(m);
     var locationSpan = (<span>N/A</span>);
+    var distanceSpan = (<span />);
     if (m.latitude) {
       locationSpan = (<span>{m.latitude}, {m.longitude}</span>);
+    }
+    if (this.state.lat) {
+      var dis = distance(m.longitude,m.latitude,this.state.lon,this.state.lat);
+      distanceSpan = (<span>, {dis + "km"}</span>);
     }
     var photoUrl = '/images/profile_placeholder.png';
     if (m.photoUrl) {
@@ -22,9 +51,8 @@ class MessageView extends Component {
     return (<div>
               <Card>
                 <CardBlock>
-                  <CardSubtitle>Geo: {locationSpan}</CardSubtitle>
+                  <CardSubtitle>Geo: {locationSpan}{distanceSpan}</CardSubtitle>
                   <CardText>Summary: {m.text}</CardText>
-          
                 </CardBlock>
                 <ProgressiveCardImg top gs_src={m.imageUrl}/>
                 <CardBlock>
