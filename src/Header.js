d552f2bde4d95415b567f131073c6b91b301a610
Ho Wa Wong <howa.wong@gmail.com>
2018-02-27 19:33:14 +0800

    Remove old location code in Header #67

--- src/Header.js
+++ src/Header.js
@@ -22,59 +22,15 @@ const styles = {
   },
 };
 
-const currentLocationLabel = "現在位置";
-
 class Header extends  Component {
   constructor(props) {
     super(props);
-    this.state = {locationString: currentLocationLabel};
-    this.setLocation = this.setLocation.bind(this);
-    this.successCallBack = this.successCallBack.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   handleLeftTouchTap() {
     console.log('Open Drawer');
     alert('onTouchTap triggered on the title component');
     this.drawerMenu.handleToggle();
-  }
-
-  componentDidMount() {
-  }
-
-  notSupportedCallBack() {
-    console.log('Disabled');
-  }
-
-  successCallBack(pos) {
-    console.log('get New Location: ' + pos.coords.longitude + "," + pos.coords.latitude);
-    this.setState({ lat: pos.coords.latitude, lon: pos.coords.longitude}); 
-    if(this.props.updateLocationCallback != null) {
-      this.props.updateLocationCallback(this.state.locationString, this.state.lon, this.state.lat);
-    }
-  }
-
-  errorCallBack(error) {
-    console.warn('ERROR(${err.code}): ${err.message}');
-  }  
-
-  setLocation(locationString, longitude, latitude) {
-    console.log("set New Location1:" + locationString);
-    if(locationString == "" || locationString == currentLocationLabel) {
-      this.setState({locationString: currentLocationLabel});
-      //getLocation(this.successCallBack, this.errorCallBack, this.notSupportedCallback);      
-    } else {
-      console.log("set New Location2:" + longitude + " " + latitude);
-      this.setState({locationString: locationString, lon: longitude, lat: latitude});
-      if(this.props.updateLocationCallback != null) {
-        this.props.updateLocationCallback(this.state.locationString, this.state.lon, this.state.lat);
-      }      
-    }
-  }
-
-  getLocation() {
-    var locationValue = {longitude: this.state.lon, latitude: this.state.lat};
-    return locationValue;
   }
 
   render() {
@@ -110,6 +66,4 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 
-
 export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Header));
-//export default Header;
