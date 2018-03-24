b7fda3748cf73ef1630c97c3c17574882e9330b1
khmak3 <pegasus.mak@gmail.com>
2018-03-25 02:11:45 +0800

    #99 Support display first tag and thumbnail

--- src/MessageView.js
+++ src/MessageView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Card, { CardHeader, CardContent} from 'material-ui/Card';
+import Card, { CardMedia, CardHeader, CardContent} from 'material-ui/Card';
 import distance from './Distance';
 import timeOffsetStringInChinese from './TimeString.js';
 import { withStyles } from 'material-ui/styles';
@@ -10,6 +10,9 @@ import Typography from 'material-ui/Typography';
 import {connect} from "react-redux";
 
 const styles = theme => ({
+  card: {
+    display: 'flex',
+},  
   avatar: {
     backgroundColor: red[500],
   },
@@ -25,6 +28,17 @@ const styles = theme => ({
 //    marginBottom: 12,
     color: theme.palette.text.secondary,
   },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+},  
+  cover: {
+    width: 64,
+    height: 64,
+  },  
 });
 
 class MessageView extends Component {
@@ -43,6 +57,11 @@ class MessageView extends Component {
     var m = this.props.message;
     var uuid = this.props.message.key;
     var user = this.props.user;
+    var imageUrl = m.thumbnailPublicImageURL;
+    if(imageUrl == null) {
+      imageUrl = m.photoUrl;
+    }
+    console.log(m + m.thumbnailPublicImageURL + m.photoUrl + imageUrl)
     //var openDialog = this.props.openDialog;
     var o = false;
     if(this.props.openDialogDefault) {
@@ -64,15 +83,25 @@ class MessageView extends Component {
     var timeOffset = Date.now() - m.createdAt;
     var timeOffsetString = timeOffsetStringInChinese(timeOffset);
     var auther = m.name + ' 於: ' + timeOffsetString + '前張貼 '
-    var subtitle = distanceSpan + ' 現況: ' + m.status;
+    var tag = '';
+    if(m.tag != null && m.tag.length > 1) {
+      tag = ' #' + m.tag[0];
+    }
+    var subtitle = distanceSpan + ' 現況: ' + m.status + tag;
 //    let summary = <CardHeader title={m.text}  subheader={subtitle}  onClick={() => this.handleClick()}>  </CardHeader>
-    let summary = <CardContent onClick={() => this.handleClick()}>
-                    <Typography className={classes.title}>{auther}</Typography>
-                      <Typography variant="headline" component="h2"> {m.text}</Typography>
-                    <Typography className={classes.pos}>{subtitle}</Typography>
-                  </CardContent>
+    let summary = <div className={classes.details}>
+                    <CardContent className={classes.content} onClick={() => this.handleClick()}>
+                      <Typography className={classes.title}>{auther}</Typography>
+                        <Typography variant="headline" component="h2"> {m.text}</Typography>
+                      <Typography className={classes.pos}>{subtitle}</Typography>
+                    </CardContent>
+                  </div> 
+    let thumbnail = <CardMedia
+                      className={classes.cover}
+                      image={imageUrl}/>
     return (<div>
-              <Card>
+              <Card className={classes.card}>
+                {thumbnail}
                 {summary}
               </Card>
               <MessageDialog uuid={uuid} user={user} open={o} openDialog={openDialog => this.openDialog = openDialog} ref={(messageDialog) => {this.messageDialog = messageDialog;}} />
