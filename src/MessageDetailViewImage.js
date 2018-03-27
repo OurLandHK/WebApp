923fe9e146b74c5171dd3ea3e58d1f6ad17848e8
wingkwong <wingkwong.code@gmail.com>
2018-03-27 10:28:58 +0800

    allowed users to add a photo if there s no photo

--- src/MessageDetailViewImage.js
+++ src/MessageDetailViewImage.js
@@ -1,10 +1,40 @@
 import ProgressiveCardImg from './ProgressiveCardImg';
 import Grid from 'material-ui/Grid';
+import {FormGroup} from 'reactstrap';
 import React, { Component } from 'react';
 import { CardActions, CardContent, CardMedia} from 'material-ui/Card';
+import UploadImageButton from './UploadImageButton';
+import Button from 'material-ui/Button';
+import {updateMessageImageURL} from './MessageDB';
+import uuid from 'js-uuid';
 
 
 class MessageDetailViewImage extends Component {
+   constructor(props) {
+        super(props);
+        var key = uuid.v4();
+        this.state = {
+            key: key,
+            imageURL: null, 
+            publicImageURL: null, 
+            thumbnailImageURL: null, 
+            thumbnailPublicImageURL: null
+        };
+    }   
+
+  uploadFinish(imageURL, publicImageURL, thumbnailImageURL, thumbnailPublicImageURL) {  
+    this.setState({
+      imageURL: imageURL, 
+      publicImageURL: publicImageURL, 
+      thumbnailImageURL: thumbnailImageURL, 
+      thumbnailPublicImageURL: thumbnailPublicImageURL
+    });
+   }
+
+   onSubmit(){
+      updateMessageImageURL(this.props.messageUUID, this.state.imageURL, this.state.publicImageURL);
+   }
+
   render() {
     
     const { url } = this.props;
@@ -16,6 +46,14 @@ class MessageDetailViewImage extends Component {
         <center>
           <br/>
           <h1>沒有圖片</h1>
+          <br/>
+          <FormGroup>  
+          <br/>
+          <UploadImageButton ref={(uploadImageButton) => {this.uploadImageButton = uploadImageButton;}} path={this.state.key} uploadFinish={(imageURL, publicImageURL, thumbnailImageURL, thumbnailPublicImageURL) => {this.uploadFinish(imageURL, publicImageURL, thumbnailImageURL, thumbnailPublicImageURL);}}/>
+          </FormGroup>
+          <Button color="contrast" onClick={() => this.onSubmit()}>
+            提交
+          </Button>
         </center>
       </div>
     );
