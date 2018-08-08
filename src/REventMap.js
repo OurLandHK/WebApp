ec29ef915604624546fd8cf1769766050e6cc450
Kalun Wong <calendarw@gmail.com>
2018-08-08 23:50:02 +0800

    Format only, no code change

--- src/REventMap.js
+++ src/REventMap.js
@@ -28,8 +28,8 @@ export class EventMap extends Component {
   }
 
   onMapClicked(mapProps, map, clickEvent) {
-    console.log('mapProps',mapProps)
-    let newPosSet = {lat: clickEvent.latLng.lat(), lng: clickEvent.latLng.lng()};
+    console.log('mapProps', mapProps)
+    let newPosSet = { lat: clickEvent.latLng.lat(), lng: clickEvent.latLng.lng() };
     this.state.center = newPosSet;
     mapProps.google.maps.Marker;
   }
@@ -37,45 +37,45 @@ export class EventMap extends Component {
   render() {
     const MyMapComponent = compose(
       withProps({
-          googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDdPxqSdKSWLot9NS0yMD2CQtI1j4GF_Qo&v=3.exp&libraries=geometry,drawing,places",
-          loadingElement: <div style={{ height: `100%` }} />,
-          containerElement: <div style={{ height: `40vh` }} />,
-          mapElement: <div style={{ height: `100%` }} />,
-        }),
-        withState('zoom', 'onZoomChange', 8),
-        withHandlers(() => {
-          const refs = {
-            map: undefined,
-          }
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDdPxqSdKSWLot9NS0yMD2CQtI1j4GF_Qo&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `40vh` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+      }),
+      withState('zoom', 'onZoomChange', 8),
+      withHandlers(() => {
+        const refs = {
+          map: undefined,
+        }
 
-          return {
-            onMapMounted: () => ref => {
-              refs.map = ref
-            },
-            onClick: (wt) => {
-              console.log(refs.map.getCenter())
-            },
-            onZoomChanged: ({ onZoomChange }) => () => {
-              onZoomChange(refs.map.getZoom())
-            }
+        return {
+          onMapMounted: () => ref => {
+            refs.map = ref
+          },
+          onClick: (wt) => {
+            console.log(refs.map.getCenter())
+          },
+          onZoomChanged: ({ onZoomChange }) => () => {
+            onZoomChange(refs.map.getZoom())
           }
-        }),
-        withScriptjs,
-        withGoogleMap
-      )((props) =>
+        }
+      }),
+      withScriptjs,
+      withGoogleMap
+    )((props) =>
       <GoogleMap
-      defaultZoom={this.props.zoom}
-      defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+        defaultCenter={this.props.center}
       >
-      <Marker position={this.props.center} onClick={props.onMarkerClick} />
+        <Marker position={this.props.center} onClick={props.onMarkerClick} />
       </GoogleMap>
     );
-      return (
-        <CardMedia className="map-wrapper">
-          <MyMapComponent/>
-        </CardMedia>
-      );
-    }
+    return (
+      <CardMedia className="map-wrapper">
+        <MyMapComponent />
+      </CardMedia>
+    );
+  }
 }
 
 export default EventMap
