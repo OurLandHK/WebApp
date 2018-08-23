d0388cc0c008495ae1690279a97e770cf00485a0
wingkwong <wingkwong.code@gmail.com>
2018-08-23 22:37:51 +0800

    added fetchMessagesBasedOnInterestedTags

--- src/NotificationsDialog.js
+++ src/NotificationsDialog.js
@@ -17,7 +17,7 @@ import MessageList from './MessageList';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Divider from '@material-ui/core/Divider';
 import FilterBar from './FilterBar';
-import {fetchMessagesBaseOnGeo, fetchReportedUrgentMessages} from './MessageDB';
+import {fetchMessagesBaseOnGeo, fetchReportedUrgentMessages, fetchMessagesBasedOnInterestedTags} from './MessageDB';
 import {
   toggleAddressDialog,
 } from "./actions";
@@ -112,10 +112,13 @@ class NotificationsDialog extends React.Component {
         if(user.userProfile.role == RoleEnum.admin) {
           distance = 100;
         }
-        //console.log(address.geolocation);
         fetchMessagesBaseOnGeo(address.geolocation, distance, constant.defaultEventNumber, lastLoginTime, null, this.setMessage);
         if(user.userProfile.role == RoleEnum.admin || user.userProfile.role == RoleEnum.monitor) {
           fetchReportedUrgentMessages(this.setMessage);
+        }
+
+        if(user.userProfile.interestedTags != null && user.userProfile.interestedTags.length > 0) {
+          fetchMessagesBasedOnInterestedTags(user.userProfile.interestedTags, address.geolocation, distance, this.setMessage);
         }
       }
     });      
