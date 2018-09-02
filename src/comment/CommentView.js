e5a1d15b4c347ea04c604aaa2eb6ade9ab87ccee
khmak3 <pegasus.mak@gmail.com>
2018-09-02 18:35:59 +0800

    #207 fix cardactionarea issue.

--- src/comment/CommentView.js
+++ src/comment/CommentView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
