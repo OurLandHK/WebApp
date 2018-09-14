0eaeadbe3e60fa0711be4ab9818134b1da59295c
wingkwong <wingkwong.code@gmail.com>
2018-09-14 22:35:48 +0800

    #338 added CustomizedSnackbars

---
+++ src/CustomizedSnackbars.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Button from '@material-ui/core/Button';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import green from '@material-ui/core/colors/green';
+import amber from '@material-ui/core/colors/amber';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import { withStyles } from '@material-ui/core/styles';
+import {connect} from "react-redux";
+
+
+import {openSnackbar, closeSnackbar} from "./actions";
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  warning: WarningIcon,
+  error: ErrorIcon,
+  info: InfoIcon,
+};
+
+const MySnackbarContentStyle = theme => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  warning: {
+    backgroundColor: amber[700],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+function MySnackbarContent(props) {
+  const { classes, className, message, onClose, variant, ...other } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={classNames(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={classNames(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+          onClick={onClose}
+        >
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
+  );
+}
+
+MySnackbarContent.propTypes = {
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  message: PropTypes.node,
+  onClose: PropTypes.func,
+  variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
+};
+
+const MySnackbarContentWrapper = withStyles(MySnackbarContentStyle)(MySnackbarContent);
+
+const CustomizedSnackbarsStyle = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class CustomizedSnackbars extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.props.closeSnackbar();
+  };
+
+  render() {
+    const { classes, snackbar } = this.props;
+    return (
+      <div><Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={snackbar.open}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+        >
+           <MySnackbarContentWrapper
+             variant={snackbar.variant}
+             onClose={this.handleClose}
+             message={snackbar.message}
+          />
+        </Snackbar>
+      </div>
+    );
+  }
+}
+
+CustomizedSnackbars.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    snackbar: state.snackbar
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    openSnackbar: (open, message, variant) => dispatch(openSnackbar(open, message, variant)),
+    closeSnackbar: () => dispatch(closeSnackbar())
+  }
+};
+
+export default withStyles(CustomizedSnackbarsStyle)(connect(mapStateToProps, mapDispatchToProps)(CustomizedSnackbars));
