beef3d6894e231db433454647b663dbba698b5dd
khmak3 <pegasus.mak@gmail.com>
2018-09-30 22:17:11 +0800

    Minor bug fix for recent

--- src/Main.js
+++ src/Main.js
@@ -99,13 +99,13 @@ class Main extends Component {
     const { classes } = this.props;
     let tagStatHtml = this.renderTagStat();
 
-    if (queryMessage && open) {
+    if (queryMessage && openRecent) {
       let message = queryMessage;
       recentMessage = <div className="recent-event-wrapper">
                         <h4>{constant.recentEventLabel}</h4>
                         <MessageView message={message} key={message.key} openDialogDefault={openRecent} />
                       </div>;
-    } else if (bookmark  != null && open) {
+    } else if (bookmark && openRecent) {
       recentMessage = <div className="recent-event-wrapper">
                         <h4>{constant.recentEventLabel}</h4>
                         <BookmarkView bookmark={bookmark} open={openRecent} />
