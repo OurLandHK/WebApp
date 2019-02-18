24493a38d06e12cc129f40601e81a9596ee36660
khmak3 <pegasus.mak@gmail.com>
2019-02-19 00:25:33 +0800

    Fix front page issue

--- src/bookmark/BookmarkList.js
+++ src/bookmark/BookmarkList.js
@@ -12,11 +12,12 @@ class BookmarkList extends Component {
       this.limitLength = this.props.limitLength
     }
     this.state = {
-      bookmarkList: this.props.bookmarkList
+      bookmarkList: []
     }
   }
 
   componentDidMount() {
+    console.log(this.limitLength);
     if(this.props.bookmarkList !== undefined && this.props.bookmarkList !== null) {
       let bookmarkList = [];
       if(this.limitLength === 0) {
@@ -24,17 +25,23 @@ class BookmarkList extends Component {
       } else {
         bookmarkList = this.props.bookmarkList.slice(0, this.limitLength);
       }
-      this.setState({ bookmarkList });
+      this.setState({ bookmarkList: bookmarkList });
     }
   }
 
   render() {
     let elements = null;
     let bookmarkList = this.state.bookmarkList;
-    if(bookmarkList !== undefined && this.props.bookmarkList !== undefined && bookmarkList.length < this.props.bookmarkList.length) {
-      bookmarkList = this.props.bookmarkList;
+    console.log(bookmarkList.length);
+    if(bookmarkList !== undefined && this.props.bookmarkList !== undefined && bookmarkList.length === 0) {
+      if(this.limitLength === 0) {
+        bookmarkList = this.props.bookmarkList;
+      } else {
+        bookmarkList = this.props.bookmarkList.slice(0, this.limitLength);
+      }
     }
-    if(bookmarkList !== undefined) {
+    console.log(bookmarkList.length);
+    if(bookmarkList.length !== 0) {
         elements = bookmarkList.map((bookmark) => {
             return (<BookmarkView bookmark={bookmark} OnChange={() => {this.componentWillMount()}}/>);
         });
